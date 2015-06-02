bb63827cafb4fe549a2d7584102875a0f0451888
r1n0xmk2 <rene7297152@gmail.com>
2015-06-02 18:18:39 +0800

    test brief, start detail

---
+++ getCompanyBrief.js
@@ -0,0 +1,189 @@
+var fs = require('fs');
+var moment = require('moment');
+var async = require('async')
+var cheerio = require('cheerio');
+var _ = require('lodash');
+var _s = require('underscore.string');
+
+var request = require('./tools/tools.js').tryRequest;
+var tools = require('./tools/tools.js');
+var pg = require('./tools/pg.js');
+var redis = require('./tools/redis.js');
+
+var insertBriefSql = 'INSERT INTO alibaba_company (name, sid, url, gold_supplier, assurance, update_date, status) '
+              + 'VALUES ($1, $2, $3, $4, $5, $6, $7);'
+var checkSidSql = 'SELECT id, name, sid, url FROM alibaba_company WHERE sid = $1';
+var has = true;
+var REDIS_KEY = 'alibaba_category_key';
+
+async.whilst(function () {
+  return has;
+}, function (callback) {
+  redis.spop(REDIS_KEY, function (err, url) {
+    if (url) {
+      console.log("+++++++++++++++++++++",url, moment().utc().format());
+      request({
+        url: url,
+        headers: {
+          'User-Agent': 'request'
+        }
+      }, function (err, res, data) {
+        if (err || res.statusCode != 200) {
+          console.log('eachReqError',err);
+          errList.push(url);
+          callback();
+        } else {
+          $ = cheerio.load(data);
+          times = $('a.next').prev().html() || 1;
+          var count = 0;
+          async.whilst(
+            function () { return count < times; },
+            function (cbEachPage) {
+              count++;
+              request({
+                url: url + '/' + count,
+                headers: {
+                  'User-Agent': 'request'
+                }
+              }, catchCompanyListEachPageAfterRequest(url, count, cbEachPage));
+            },
+            function (err) {
+              if(err) {
+                redis.sadd(REDIS_KEY, url, function(){
+                  callback();
+                });
+              } else {
+                callback();
+              }
+            }
+          );
+        }
+      })
+    } else {
+      has = false;
+      callback();
+    }
+  });
+}, function (err) {
+  redis.end();
+  console.log('All Done.',new Date());
+});
+
+function catchCompanyListEachPageAfterRequest (url, count, cbEachPage) {
+  return function (err, res, data) {
+    console.log("---------------------", url + '/' + count, moment().utc().format());
+    if (err || res.statusCode != 200) {
+      console.log('>>>>>>>>>>>>>>>>>>>>>whileReqError', moment().utc().format(),err);
+      console.log(err|| res.statusCode);
+      count--;
+      cb();
+    }
+    else {
+      async.each(catchCompanyList(data), function(company, cbEachCompany) {
+        checkCompanyExist(company, function(flag) {
+          if(flag) cbEachCompany();
+          else {
+            company.push(moment().utc().format('YYYY-MM-DD HH:mm:ss'));
+            company.push('brief');
+            pg.query(insertBriefSql, company, function (err, result) {
+              if(err) console.log(err);
+              else {
+                cbEachCompany();
+              }
+            })
+          }
+        })
+      },
+      function (err) {
+          if(err) console.log(err);
+          console.log("---------------------",url + '/' + count, 'end')
+          cbEachPage();
+      })
+      // async.filter(catchCompanyList(data), checkNotCompanyExist, function(companys) {
+      //   async.each(companys, function(company, cbEachCompany) {
+      //     request(
+      //       {
+      //       url: company[2],
+      //       headers: {
+      //         'User-Agent': 'request'
+      //       }
+      //     },
+      //     catchCompanyDetailAfterRequest(company, cbEachCompany));
+      //   },
+      //   function (err) {
+      //       if(err) console.log(err);
+      //       console.log("---------------------",url + '/' + count, 'end')
+      //       cbEachPage();
+      //   })
+      // })
+    }
+  }
+}
+
+// function catchCompanyDetailAfterRequest (company, cbEachCompany) {
+//   return function (err, res, data) {
+//     console.log("=====================",company[1], moment().utc().format());
+//     if (err || res.statusCode != 200) {
+//       console.log('>>>>>>>>>>>>>>>>>>>>>detailReqError', company[1], moment().utc().format(),err);
+//       company.push(moment().utc().format('YYYY-MM-DD HH:mm:ss'));
+//       company.push('err');
+//       pg.query(insertErrSql, company, function (err, result) {
+//         if(err) console.log(err);
+//         else {
+//           console.log('inserted;');
+//           cbEachCompany();
+//         }
+//       });
+//     }
+//     else {
+//       catchCompanyDetailAndPush(data, company);
+//       pg.query(insertSql, company, function (err, result) {
+//         if(err) console.log(err);
+//         else {
+//           cbEachCompany();
+//         }
+//       });
+//     }
+//   }
+// }
+
+function catchCompanyList(data) {
+	var $ = cheerio.load(data);
+	var result = [];
+	$('#J-items-content>div.f-icon.m-item').each( function(i, li) {
+    result.push([
+			tools.convertHTMLEntity($('div.item-title .title.ellipsis>a',li).html()),
+			Number($('h2.title.ellipsis>a', li).attr('data-hislog')),
+			tools.getContact($('div.item-title .title.ellipsis>a',li).attr('href')),
+			$('.ico-year>span', li).length&&/\d+/.test($('.ico-year>span').attr('class'))?Number(/\d+/.exec($('.ico-year>span').attr('class'))[0]):0,
+			$('.ico-ta', li).length? true:false
+		]);
+	})
+	return result;
+}
+
+function checkCompanyExist (company, callback) {
+  pg.query(checkSidSql, [company[1]], function(err, result){
+    if(result.rowCount > 0) {
+      callback(true);
+    } else {
+      callback(false);
+    }
+  })
+}
+
+function catchCompanyDetailAndPush(data, company) {
+  var $ = cheerio.load(data);
+  var contact = {
+    person: _s.clean($('div.contact-overview>div.contact-info>h1.name').text())
+  };
+  $('div.contact-overview>div.contact-info>dl>dt').each(function(i, dt) {
+    contact[$(dt).text().replace(':','')] = $(dt).next('dd').text();
+  });
+  $('div.contact-detail>dl>dt').each(function(i, dt) {
+    contact[$(dt).text().replace(':','')] = $(dt).next('dd').text();
+  });
+  company.push(moment().utc().format('YYYY-MM-DD HH:mm:ss'));
+  company.push('suc');
+  company.push(JSON.stringify(contact));
+}
--- getCompanyDetail.js
+++ getCompanyDetail.js
@@ -9,106 +9,45 @@ var request = require('./tools/tools.js').tryRequest;
 var tools = require('./tools/tools.js');
 var pg = require('./tools/pg.js');
 var redis = require('./tools/redis.js');
-
-var readFileName = process.argv[2] || './data/catList.json';
-var errFileName = process.argv[2] || './data/catListErr.json';
-var insertSql = 'INSERT INTO alibaba_company (name, sid, url, gold_supplier, assurance, update_date, status, contact) '
-              + 'VALUES ($1, $2, $3, $4, $5, $6, $7, $8);'
-var insertErrSql = 'INSERT INTO alibaba_company (name, sid, url, gold_supplier, assurance, update_date, status) '
-              + 'VALUES ($1, $2, $3, $4, $5, $6, $7);'
+var getRandomSql = 'SELECT id, name, sid, url, status FROM alibaba_company WHERE status = \'brief\' ORDER BY RANDOM() LIMIT 1;';
+var updateDetail = 'UPDATE alibaba_company SET contact = $1, update_data = $2, status = $3 where id = $4';
 var checkSidSql = 'SELECT id, name, sid, url FROM alibaba_company WHERE sid = $1';
-var has = true;
+var has = 3;
+// var has = true;
 var REDIS_KEY = 'alibaba_category_key';
 
 async.whilst(function () {
-  return has;
+  return --has;
 }, function (callback) {
-  redis.spop(REDIS_KEY, function (err, url) {
-    if (url) {
-      console.log("+++++++++++++++++++++",url, moment().utc().format());
+  pg.query(getRandomSql, [], function(err, result) {
+    if(result.rows.length === 1) {
       request({
-        url: url,
+        url: rows.url,
         headers: {
           'User-Agent': 'request'
         }
-      }, function (err, res, data) {
-        if (err || res.statusCode != 200) {
-          console.log('eachReqError',err);
-          errList.push(url);
-          callback();
-        } else {
-          $ = cheerio.load(data);
-          times = $('a.next').prev().html() || 1;
-          var count = 0;
-          async.whilst(
-            function () { return count < times; },
-            function (cbEachPage) {
-              count++;
-              request({
-                url: url + '/' + count,
-                headers: {
-                  'User-Agent': 'request'
-                }
-              }, catchCompanyListEachPageAfterRequest(url, count, cbEachPage));
-            },
-            function (err) {
-              if(err) {
-                redis.sadd(REDIS_KEY, url, function(){
-                  callback();
-                });
-              } else {
-                callback();
-              }
-            }
-          );
-        }
-      })
+      },
+      catchCompanyDetailAfterRequest(result.rows, callback));
+
     } else {
       has = false;
-      callback();
+      callback()
     }
-  });
+    console.log(result.rows);
+    callback()
+  })
 }, function (err) {
-  redis.end();
-  console.log('All Done.',new Date());
+  pg.end();
 });
 
-function catchCompanyListEachPageAfterRequest (url, count, cbEachPage) {
-  return function (err, res, data) {
-    console.log("---------------------", url + '/' + count, moment().utc().format());
-    if (err || res.statusCode != 200) {
-      console.log('>>>>>>>>>>>>>>>>>>>>>whileReqError', moment().utc().format(),err);
-      console.log(err|| res.statusCode);
-      count--;
-      cb();
-    }
-    else {
-      async.filter(catchCompanyList(data), checkNotCompanyExist, function(companys) {
-        async.each(companys, function(company, cbEachCompany) {
-          request(
-            {
-            url: company[2],
-            headers: {
-              'User-Agent': 'request'
-            }
-          },
-          catchCompanyDetailAfterRequest(company, cbEachCompany));
-        },
-        function (err) {
-            if(err) console.log(err);
-            console.log("---------------------",url + '/' + count, 'end')
-            cbEachPage();
-        })
-      })
-    }
-  }
-}
+
+
 
 function catchCompanyDetailAfterRequest (company, cbEachCompany) {
   return function (err, res, data) {
-    console.log("=====================",company[1], moment().utc().format());
+    console.log("=====================",company.url, moment().utc().format());
     if (err || res.statusCode != 200) {
-      console.log('>>>>>>>>>>>>>>>>>>>>>detailReqError', company[1], moment().utc().format(),err);
+      console.log('>>>>>>>>>>>>>>>>>>>>>detailReqError', company.url, moment().utc().format(),err);
       company.push(moment().utc().format('YYYY-MM-DD HH:mm:ss'));
       company.push('err');
       pg.query(insertErrSql, company, function (err, result) {
@@ -120,6 +59,9 @@ function catchCompanyDetailAfterRequest (company, cbEachCompany) {
       });
     }
     else {
+      var insertData = [
+
+      ]
       catchCompanyDetailAndPush(data, company);
       pg.query(insertSql, company, function (err, result) {
         if(err) console.log(err);
@@ -131,44 +73,153 @@ function catchCompanyDetailAfterRequest (company, cbEachCompany) {
   }
 }
 
-function catchCompanyList(data) {
-	var $ = cheerio.load(data);
-	var result = [];
-	$('#J-items-content>div.f-icon.m-item').each( function(i, li) {
-    result.push([
-			tools.convertHTMLEntity($('div.item-title .title.ellipsis>a',li).html()),
-			Number($('h2.title.ellipsis>a', li).attr('data-hislog')),
-			tools.getContact($('div.item-title .title.ellipsis>a',li).attr('href')),
-			$('.ico-year>span', li).length&&/\d+/.test($('.ico-year>span').attr('class'))?Number(/\d+/.exec($('.ico-year>span').attr('class'))[0]):0,
-			$('.ico-ta', li).length? true:false
-		]);
-	})
-	return result;
-}
 
-function checkNotCompanyExist (company, callback) {
-  pg.query(checkSidSql, [company[1]], function(err, result){
-    if(result.rowCount > 0) {
-      callback(false);
-    } else {
-      console.log('havent')
-      callback(true);
-    }
-  })
-}
 
-function catchCompanyDetailAndPush(data, company) {
-  var $ = cheerio.load(data);
-  var contact = {
-    person: _s.clean($('div.contact-overview>div.contact-info>h1.name').text())
-  };
-  $('div.contact-overview>div.contact-info>dl>dt').each(function(i, dt) {
-    contact[$(dt).text().replace(':','')] = $(dt).next('dd').text();
-  });
-  $('div.contact-detail>dl>dt').each(function(i, dt) {
-    contact[$(dt).text().replace(':','')] = $(dt).next('dd').text();
-  });
-  company.push(moment().utc().format('YYYY-MM-DD HH:mm:ss'));
-  company.push('suc');
-  company.push(JSON.stringify(contact));
-}
+// async.whilst(function () {
+//   return has;
+// }, function (callback) {
+//   redis.spop(REDIS_KEY, function (err, url) {
+//     if (url) {
+//       console.log("+++++++++++++++++++++",url, moment().utc().format());
+//       request({
+//         url: url,
+//         headers: {
+//           'User-Agent': 'request'
+//         }
+//       }, function (err, res, data) {
+//         if (err || res.statusCode != 200) {
+//           console.log('eachReqError',err);
+//           errList.push(url);
+//           callback();
+//         } else {
+//           $ = cheerio.load(data);
+//           times = $('a.next').prev().html() || 1;
+//           var count = 0;
+//           async.whilst(
+//             function () { return count < times; },
+//             function (cbEachPage) {
+//               count++;
+//               request({
+//                 url: url + '/' + count,
+//                 headers: {
+//                   'User-Agent': 'request'
+//                 }
+//               }, catchCompanyListEachPageAfterRequest(url, count, cbEachPage));
+//             },
+//             function (err) {
+//               if(err) {
+//                 redis.sadd(REDIS_KEY, url, function(){
+//                   callback();
+//                 });
+//               } else {
+//                 callback();
+//               }
+//             }
+//           );
+//         }
+//       })
+//     } else {
+//       has = false;
+//       callback();
+//     }
+//   });
+// }, function (err) {
+//   redis.end();
+//   console.log('All Done.',new Date());
+// });
+//
+// function catchCompanyListEachPageAfterRequest (url, count, cbEachPage) {
+//   return function (err, res, data) {
+//     console.log("---------------------", url + '/' + count, moment().utc().format());
+//     if (err || res.statusCode != 200) {
+//       console.log('>>>>>>>>>>>>>>>>>>>>>whileReqError', moment().utc().format(),err);
+//       console.log(err|| res.statusCode);
+//       count--;
+//       cb();
+//     }
+//     else {
+//       async.each(companys, function(company, cbEachCompany) {
+//         checkCompanyExist(company, function(flag) {
+//           if(flag) cbEachCompany();
+//           else {
+//             company.push(moment().utc().format('YYYY-MM-DD HH:mm:ss'));
+//             company.push('brief');
+//             pg.query(insertBriefSql, company, function (err, result) {
+//               if(err) console.log(err);
+//               else {
+//                 console.log('inserted;');
+//                 cbEachCompany();
+//               }
+//             })
+//           }
+//         })
+//       },
+//         function (err) {
+//             if(err) console.log(err);
+//             console.log("---------------------",url + '/' + count, 'end')
+//             cbEachPage();
+//         })
+//       })
+      // async.filter(catchCompanyList(data), checkNotCompanyExist, function(companys) {
+      //   async.each(companys, function(company, cbEachCompany) {
+      //     request(
+      //       {
+      //       url: company[2],
+      //       headers: {
+      //         'User-Agent': 'request'
+      //       }
+      //     },
+      //     catchCompanyDetailAfterRequest(company, cbEachCompany));
+      //   },
+      //   function (err) {
+      //       if(err) console.log(err);
+      //       console.log("---------------------",url + '/' + count, 'end')
+      //       cbEachPage();
+      //   })
+      // })
+//     }
+//   }
+// }
+
+
+// function catchCompanyList(data) {
+// 	var $ = cheerio.load(data);
+// 	var result = [];
+// 	$('#J-items-content>div.f-icon.m-item').each( function(i, li) {
+//     result.push([
+// 			tools.convertHTMLEntity($('div.item-title .title.ellipsis>a',li).html()),
+// 			Number($('h2.title.ellipsis>a', li).attr('data-hislog')),
+// 			tools.getContact($('div.item-title .title.ellipsis>a',li).attr('href')),
+// 			$('.ico-year>span', li).length&&/\d+/.test($('.ico-year>span').attr('class'))?Number(/\d+/.exec($('.ico-year>span').attr('class'))[0]):0,
+// 			$('.ico-ta', li).length? true:false
+// 		]);
+// 	})
+// 	return result;
+// }
+//
+// function checkCompanyExist (company, callback) {
+//   pg.query(checkSidSql, [company[1]], function(err, result){
+//     if(result.rowCount > 0) {
+//       callback(false);
+//     } else {
+//       console.log('havent')
+//       callback(true);
+//     }
+//   })
+// }
+//
+// function catchCompanyDetailAndPush(data, company) {
+//   var $ = cheerio.load(data);
+//   var contact = {
+//     person: _s.clean($('div.contact-overview>div.contact-info>h1.name').text())
+//   };
+//   $('div.contact-overview>div.contact-info>dl>dt').each(function(i, dt) {
+//     contact[$(dt).text().replace(':','')] = $(dt).next('dd').text();
+//   });
+//   $('div.contact-detail>dl>dt').each(function(i, dt) {
+//     contact[$(dt).text().replace(':','')] = $(dt).next('dd').text();
+//   });
+//   company.push(moment().utc().format('YYYY-MM-DD HH:mm:ss'));
+//   company.push('suc');
+//   company.push(JSON.stringify(contact));
+// }
